fef8c7b866735ecc1f8bb7eb323e4efb5ea8f207
Erick <ericksjp703@gmail.com>
2023-10-12 13:34:38 -0300

    Completing Caesar Cipher Algorithm

---
+++ ruby_projects/01.ceasar_cipher.rb
@@ -0,0 +1,13 @@
+def caesar_cipher(string, number = 0)
+    result = string.split("")
+    result.map! do |letter|
+        newLetter = letter.ord
+        if ('a'..'z').include?(letter)
+            newLetter = ((newLetter + number - 'a'.ord) % 26 + 'a'.ord)
+        elsif ('A'..'Z').include?(letter)
+            newLetter = ((newLetter + number - 'A'.ord) % 26 + 'A'.ord)
+        end
+        newLetter.chr
+    end
+    return result.join("");
+end
